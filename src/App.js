a61a2098b27eafc738103a4947036e8ae2e767e2
OzanOcak <ozanocak@gmail.com>
2022-09-06 15:37:25 -0400

    users and posts reducers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { Counter } from "./features/counter/Counter";
+import AddPostForm from "./features/posts/AddPostForm";
+import PostsList from "./features/posts/PostList";
 
 function App() {
   return (
     <>
       <main className="App">
-        {" "}
-        <Counter />
+        <AddPostForm />
+        <PostsList />
       </main>
     </>
   );
