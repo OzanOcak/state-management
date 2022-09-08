0c156e7a995ed158b6e8a74008743f0e63f81463
OzanOcak <ozanocak@gmail.com>
2022-09-08 00:28:42 -0400

    router and controllers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostForm";
-import PostsList from "./features/posts/PostList";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import EditPostForm from "./features/posts/EditPostForm";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <main className="App">
-        <AddPostForm />
-        <PostsList />
-      </main>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
